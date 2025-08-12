eb4abe5e0927e0fb58a6ed80dcd89ae0c778e6cc
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-08-12 19:31:28 +0100

    set up error

--- vendor/laravel/breeze/stubs/inertia-vue/vite.config.js
+++ vendor/laravel/breeze/stubs/inertia-vue/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
     plugins: [
